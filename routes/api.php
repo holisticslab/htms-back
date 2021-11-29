ac6d20b8cbdbff042990caf19a64a015030d60c8
mikr96 <irsyadkamil96@gmail.com>
2021-11-29 15:24:14 +0800

    Fix route api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
@@ -19,14 +18,14 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // PUBLIC API
 Route::get('/ping', [HomeController::class, 'ping']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // PRIVATE API (TOKEN IS COMPULSORY)
 Route::group(['middleware' => ['auth:sanctum']], function () {
@@ -34,8 +33,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile/{id}', [ProfileController::class, 'show']);
     Route::put('/profile/{id}', [ProfileController::class, 'update']);
     Route::post("/logout",[AuthController::class,'logout']);
-    Route::get('/paginated_posts', [PostController::class, 'getPaginatedPosts']);
-    Route::apiResource('/posts', PostController::class);
     Route::get('/getcourses', [CourseController::class, 'GetCourseDetails']);
     Route::post('/postcourse', [CourseController::class, 'PostCourseDetails']);
 
